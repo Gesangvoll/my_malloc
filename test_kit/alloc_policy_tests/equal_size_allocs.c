e2e181e9a00ff683bc18a5b5aa30300559218272
cj162 <cj162@duke.edu>
2019-01-24 17:59:35 -0500

    final

--- test_kit/alloc_policy_tests/equal_size_allocs.c
+++ test_kit/alloc_policy_tests/equal_size_allocs.c
@@ -20,7 +20,6 @@
 double calc_time(struct timespec start, struct timespec end) {
   double start_sec = (double)start.tv_sec*1000000000.0 + (double)start.tv_nsec;
   double end_sec = (double)end.tv_sec*1000000000.0 + (double)end.tv_nsec;
-
   if (end_sec < start_sec) {
     return 0;
   } else {
